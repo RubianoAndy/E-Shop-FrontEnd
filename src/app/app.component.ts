098c16aa66b851abd3c49acf567b5d30d3715638
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2025-03-26 17:18:09 -0500

    feat: adding footer to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,85 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { FooterComponent } from './features/components/shared/components/footer/footer.component';
+import { filter } from 'rxjs';
+import { environment } from '../environments/environment.development';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    // AlertComponent,
+    // LoadingComponent,
+    // HeaderComponent,
+    // NavbarComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'e-shop-frontend';
+export class AppComponent implements OnInit {
+  isHeaderVisible = true;
+  isFooterVisible = true;
+
+  headerExcludedRoutes = [
+    '/sign-in',
+    '/sign-up',
+    '/recovery-password',
+    '/activate',
+    '/no-connection',
+  ];
+
+  footerExcludedRoutes = [
+    ...this.headerExcludedRoutes,
+    '/account'
+  ];
+
+  constructor (
+    private router: Router,
+    // private titleService: TitleService,
+    // private networkService: NetworkService,
+  ) { }
+
+  ngOnInit(): void {
+    this.setupTitle();
+    this.setupTheme();
+
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.isHeaderVisible = !this.headerExcludedRoutes.some(route => event.url.includes(route));
+        this.isFooterVisible = !this.footerExcludedRoutes.some(route => event.url.includes(route));
+        window.scrollTo(0, 0);  // Esta parte permite que los links del footer redirijan mostrando la parte alta de la página
+      });
+      
+    // this.setupNetwork();
+  }
+
+  setupTitle() {
+    const siteName = environment.siteName;
+    // this.titleService.setTitle(siteName);
+  }
+
+  setupTheme() {
+    const theme = localStorage.getItem('theme');
+
+    if (theme) {
+      if (theme === 'dark')
+        document.documentElement.classList.add('dark');
+      else 
+        document.documentElement.classList.remove('dark');
+    } else {
+      localStorage.setItem('theme', 'dark');
+      document.documentElement.classList.add('dark');
+    }
+  }
+
+  setupNetwork() {
+    /* this.networkService.isOnline$.subscribe(isOnline => {
+      if (!isOnline)
+        this.router.navigate(['/no-connection']);
+      else
+        this.router.navigate(['/']);
+    }); */
+  }
 }
---
+++ src/app/features/components/shared/components/footer/footer.component.ts
@@ -0,0 +1,38 @@
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { environment } from '../../../../../../environments/environment.development';
+import { Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-footer',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent implements OnInit, OnDestroy {
+  logo = environment.darkLogo;
+  page = environment.siteName;
+  currentYear = environment.currentYear;
+  email = environment.email;
+
+  private unsubscribe$ = new Subject<void>();
+
+  constructor (
+    // private darkModeService: DarkModeService,
+    private changeDetectorRef: ChangeDetectorRef,
+  ) { }
+
+  ngOnInit(): void {
+    /* this.darkModeService.darkMode$.subscribe(darkMode => {
+      this.logo = darkMode ? environment.lightLogo : environment.darkLogo;
+      this.changeDetectorRef.detectChanges();   // Forzar a la detención del cambio del logo en el ciclo de vida
+    }); */
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+}
