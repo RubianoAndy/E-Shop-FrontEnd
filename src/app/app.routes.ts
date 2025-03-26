928320071f052e882e7dcb115a20c8ac2857a814
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2025-03-26 16:34:06 -0500

    feat: adding home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,45 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '', loadComponent: () => import('./features/components/home/home.component')
+    },
+    /* { 
+        path: 'sign-in', loadComponent: () => import('./core/components/sign-in/sign-in.component'),
+        canActivate: [authGuard], data: { requiresAuth: false }
+    }, */
+    /* { 
+        path: 'sign-up', loadComponent: () => import('./core/components/sign-up/sign-up.component'),
+        canActivate: [authGuard], data: { requiresAuth: false }
+    }, */
+    /* { 
+        path: 'recovery-password', loadComponent: () => import('./core/components/recovery-password/recovery-password.component'),
+        canActivate: [authGuard], data: { requiresAuth: false }
+    }, */
+    /* { 
+        path: 'activate', loadComponent: () => import('./core/components/activate-account/activate-account.component'),
+        canActivate: [authGuard], data: { requiresAuth: false }
+    }, */
+    /* {
+        path: 'account', loadChildren: () => import('./account/account.routes'),
+        canActivate: [authGuard], data: { requiresAuth: true, breadcrumb: 'Cuenta' }
+    }, */
+    /* {
+        path: 'faq', loadComponent: () => import('./features/components/faq/faq.component'),
+    }, */
+    /* { 
+        path: 'privacy-policy', loadComponent: () => import('./features/components/privacy-policy/privacy-policy.component'),
+    }, */
+    /* { 
+        path: 'terms-and-conditions', loadComponent: () => import('./features/components/terms-and-conditions/terms-and-conditions.component')
+    }, */
+    /* {
+        path: 'contact', loadComponent: () => import('./features/components/contact/contact.component')
+    }, */
+    /* {
+        path: 'no-connection', loadComponent: () => import('./shared/components/no-connection/no-connection.component')
+    }, */
+    { 
+        path: '**', redirectTo: '', pathMatch: 'full' 
+    },
+];
