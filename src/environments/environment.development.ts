43713299b3c33b6209bd704f4d1512ee5dc99ac2
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2025-03-26 16:22:41 -0500

    feat: adding the environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: false,
+
+    siteName: 'Andy Rubiano™ - Plantas carnívoras',
+    email: 'ar.andy.rubiano@gmail.com',
+    url: 'http://localhost:4200/',
+    apiUrl: 'http://localhost:3000',
+    currentYear: new Date().getFullYear(),
+    
+    darkLogo: 'assets/images/logos/Logo carnivorous plants division black.png',
+    lightLogo: 'assets/images/logos/Logo carnivorous plants division white.png',
+
+    superAdminRole: 1,
+    adminRole: 2,
+};
