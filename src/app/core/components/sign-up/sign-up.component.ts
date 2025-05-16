352d54efa055ce29afe079e8eaca97bcf823314c
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2025-05-16 13:20:21 -0500

    feat: adding sign-up

--- src/app/core/components/sign-up/sign-up.component.ts
+++ src/app/core/components/sign-up/sign-up.component.ts
@@ -1,11 +1,177 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { environment } from '../../../../environments/environment.development';
+
+import { RegisterService } from '../../services/register/register.service';
+
+import { TermsAndConditionsInformationComponent } from '../../../features/components/terms-and-conditions/terms-and-conditions-information/terms-and-conditions-information.component';
+import { PrivacyPolicyInformationComponent } from '../../../features/components/privacy-policy/privacy-policy-information/privacy-policy-information.component';
+import { passwordValidator } from '../../../shared/validators/password.validator';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    RouterLink,
+    NgClass,
+    TermsAndConditionsInformationComponent,
+    PrivacyPolicyInformationComponent,
+  ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
-export default class SignUpComponent {
+export default class SignUpComponent implements OnInit {
+  logo = environment.darkLogo;
+  form!: FormGroup;
 
+  isPasswordVisible: boolean = false;
+  isPasswordFocused: boolean = false;
+
+  isRegistered: boolean = false;
+
+  loading: boolean = false;
+
+  isModalOpen = false;
+  modalPart: 'TC' | 'PP' = 'TC';
+
+  passwordCriteria = {
+    hasUpperCase: false,
+    hasLowerCase: false,
+    hasSpecialChar: false,
+    hasNumber: false,
+    isValidLength: false
+  };
+
+  constructor(
+    private formBuilder: FormBuilder,
+    // private router: Router,
+
+    private registerService: RegisterService,
+  ) { }
+
+  ngOnInit(): void {
+    this.createForm();
+
+    this.form.get('password')?.valueChanges.subscribe(value => {
+      this.checkPasswordCriteria(value);
+    });
+  }
+
+  createForm(data: any = null) {
+    this.form = this.formBuilder.group({
+      name_1: [data?.name_1 || '', [ Validators.required, Validators.minLength(2), Validators.maxLength(20) ]],
+      lastname_1: [data?.lastname_1 || '', [ Validators.required, Validators.minLength(2), Validators.maxLength(20) ]],
+      email: [data?.email || '', [ Validators.required, Validators.minLength(6), Validators.maxLength(50), Validators.email ]],
+      password: [data?.password || '', [ Validators.required, passwordValidator() ]],
+    });
+  }
+
+  onSubmit() {
+    var body = {
+      name_1: this.toCapitalizeCase(this.form.value.name_1),
+      lastname_1: this.toCapitalizeCase(this.form.value.lastname_1),
+      email: this.form.value.email.toLowerCase(),
+      password: this.form.value.password
+    };
+
+    if (this.form.valid && body)
+      this.register(body);
+  }
+
+  register(body:any): void {
+    this.loading = true;
+
+    this.registerService.register(body).subscribe({
+      next: (response) => {
+        this.isRegistered = true;
+      },
+      error: (response) => {
+        this.loading = false;
+      }
+    });
+  }
+
+  togglePasswordVisibility(): void {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+
+  openModal(part: 'TC' | 'PP') {
+    this.modalPart = part;
+    this.isModalOpen = true;
+  }
+
+  closeModal(): void {
+    this.isModalOpen = false;
+  }
+
+  private toCapitalizeCase (text:string): string {
+    if (!text)
+      return text;
+
+    return text
+      .trim()
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(' ');
+  }
+  
+  /* private googleInitialize() {
+    google.accounts.id.initialize({
+      client_id: '408421446410-7incluce8okp0gcgg15g6v5l43pltt95.apps.googleusercontent.com',
+      callback: (resp: any) => this.handleLogin(resp)
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("google-btn"), 
+      {
+        theme: 'filled_blue',
+        size: 'large',
+        shape: 'rectangle',
+        width: 350
+      }
+    );
+  } */
+
+  /* handleLogin(response: any) {
+    if(response){
+      const payload = this.decodeToken(response.credential);
+
+      // Petición para enviar el JWT Token al Backend
+    }
+  } */
+
+  /* private decodeToken(token: string) {
+    try {
+      const base64Url = token.split('.')[1];
+      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
+      return JSON.parse(jsonPayload);
+    } catch (error) {
+      console.error('Error decoding token', error);
+      return null;
+    }
+  } */
+
+  // Método para mostrar el popover
+  onPasswordFocus(): void {
+    this.isPasswordFocused = true;
+  }
+
+  // Método para ocultar el popover
+  onPasswordBlur(): void {
+    this.isPasswordFocused = false;
+  }
+
+  checkPasswordCriteria(password: string): void {
+    this.passwordCriteria.hasUpperCase = /[A-Z]/.test(password);
+    this.passwordCriteria.hasLowerCase = /[a-z]/.test(password);
+    this.passwordCriteria.hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    this.passwordCriteria.hasNumber = /\d/.test(password);
+    this.passwordCriteria.isValidLength = password.length >= 8 && password.length <= 20;
+  }
 }
---
+++ src/app/core/services/register/register.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+// import { Router } from '@angular/router';
+import { LoadingService } from '../../../shared/services/loading/loading.service';
+import { finalize, Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(
+    private http: HttpClient,
+    // private router: Router,
+    private loadingService: LoadingService,
+  ) { }
+
+  register(body: any): Observable<any> {
+    this.loadingService.show();
+    return this.http.post(`${this.apiUrl}/register`, body).pipe(
+      finalize(() => {
+        this.loadingService.hide();
+      })
+    );
+  }
+
+  activate(token: string): Observable<any> {
+    this.loadingService.show();
+    return this.http.post(`${this.apiUrl}/activate`, { token }).pipe(
+      finalize(() => {
+        this.loadingService.hide();
+      })
+    );
+  }
+}
