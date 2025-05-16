969ad7d5be12f71dbb1391340ffdb53ea8641b98
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2025-05-16 12:49:22 -0500

    feat: adding sign-in component and auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,18 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth/auth.guard';
 
 export const routes: Routes = [
     { 
         path: '', loadComponent: () => import('./features/components/home/home.component')
     },
-    /* { 
+    { 
         path: 'sign-in', loadComponent: () => import('./core/components/sign-in/sign-in.component'),
         canActivate: [authGuard], data: { requiresAuth: false }
-    }, */
-    /* { 
+    },
+    { 
         path: 'sign-up', loadComponent: () => import('./core/components/sign-up/sign-up.component'),
         canActivate: [authGuard], data: { requiresAuth: false }
-    }, */
+    },
     /* { 
         path: 'recovery-password', loadComponent: () => import('./core/components/recovery-password/recovery-password.component'),
         canActivate: [authGuard], data: { requiresAuth: false }
