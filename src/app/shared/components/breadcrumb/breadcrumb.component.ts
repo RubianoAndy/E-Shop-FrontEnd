185261d32b3138887938cf0e736095ed48597c63
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2025-05-17 12:59:34 -0500

    feat: adding breadcrumb

---
+++ src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter } from 'rxjs';
+
+interface Breadcrumb {
+  label: string;
+  url: string;
+}
+
+@Component({
+  selector: 'app-breadcrumb',
+  imports: [
+    RouterLink,
+  ],
+  templateUrl: './breadcrumb.component.html',
+  styleUrl: './breadcrumb.component.css'
+})
+export class BreadcrumbComponent implements OnInit {
+  breadcrumbs: Breadcrumb[] = [];
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.breadcrumbs = this.createBreadcrumbs(this.activatedRoute.root);
+    });
+
+    // Llama a la función para inicializar el breadcrumb al cargar el componente
+    this.breadcrumbs = this.createBreadcrumbs(this.activatedRoute.root);
+  }
+
+  private createBreadcrumbs(route: ActivatedRoute, url: string = '', breadcrumbs: Breadcrumb[] = []): Breadcrumb[] {
+    const children: ActivatedRoute[] = route.children;
+
+    if (children.length === 0) {
+      return breadcrumbs;
+    }
+
+    for (const child of children) {
+      const routeURL: string = child.snapshot.url.map(segment => segment.path).join('/');
+      const breadcrumbLabel = child.snapshot.data['breadcrumb'] || routeURL;
+
+      if (routeURL !== '') {
+        url += `/${routeURL}`;
+        breadcrumbs.push({ label: breadcrumbLabel, url });
+      }
+
+      return this.createBreadcrumbs(child, url, breadcrumbs);
+    }
+
+    return breadcrumbs;
+  }
+}
