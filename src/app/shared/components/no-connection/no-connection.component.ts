755a3928752ecebd77d593545fe10312b154e1d5
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2025-05-17 21:13:26 -0500

    feat: adding no-connection component

---
+++ src/app/shared/components/no-connection/no-connection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-connection',
+  imports: [],
+  templateUrl: './no-connection.component.html',
+  styleUrl: './no-connection.component.css'
+})
+export default class NoConnectionComponent {
+
+}
