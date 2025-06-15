035e9edebed545acac1f30f587ca91bb4c6337e8
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2025-06-15 12:50:08 -0500

    adding subscription to darkmode in loading

--- src/app/shared/components/loading/loading.component.ts
+++ src/app/shared/components/loading/loading.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { environment } from '../../../../environments/environment.development';
 
 import { LoadingService } from '../../services/loading/loading.service';
+import { DarkModeService } from '../../services/dark-mode/dark-mode.service';
 
 @Component({
   selector: 'app-loading',
@@ -18,11 +19,18 @@ export class LoadingComponent implements OnInit, OnDestroy {
 
   constructor (
     private loadingService: LoadingService,
+    private darkModeService: DarkModeService,
+    private changeDetectorRef: ChangeDetectorRef,
   ) { }
 
   ngOnInit(): void {
     this.loadingSubscription =  this.loadingService.loading$.subscribe((loading: boolean) => {
       this.isLoading = loading;
+    });
+
+    this.darkModeService.darkMode$.subscribe(darkMode => {
+      this.logo = darkMode ? environment.lightLogo : environment.darkLogo;
+      this.changeDetectorRef.detectChanges();
     });
   }
 
