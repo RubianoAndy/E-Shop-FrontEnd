e2e7e1a85ec4415ea026b95123f7fc4b93d6ae75
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2025-06-15 21:01:22 -0500

    feat: delete navbar component from the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { TitleService } from './shared/services/title/title.service';
 import { NetworkService } from './shared/services/network/network.service';
 
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { AlertComponent } from './shared/components/alert/alert.component';
 import { LoadingComponent } from './shared/components/loading/loading.component';
 import { HeaderComponent } from './shared/components/header/header.component';
@@ -20,7 +19,6 @@ import { HeaderComponent } from './shared/components/header/header.component';
     AlertComponent,
     LoadingComponent,
     HeaderComponent,
-    NavbarComponent,
     FooterComponent,
   ],
   templateUrl: './app.component.html',
